6db6c93725c7f43ec083ad27d18d92b008609a67
Owyn <Owyn@users.noreply.github.com>
2019-03-04 18:00:51 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,28 @@
+
+module.exports = function no_guardian(d)
+{
+	let clr = 0;
+	let entered = false;
+	
+	d.hook('S_FIELD_EVENT_ON_ENTER', 'raw', () => {  
+			entered = true;
+			return false;
+	});
+	
+	d.hook('C_RETURN_TO_LOBBY', 'raw', () => {  
+			entered = false;
+	});
+	
+	d.hook('S_FIELD_POINT_INFO', 2, (event) => {       
+		if(entered && event.cleared != clr && event.cleared - 1 > event.claimed) // entered GM, updated progress, unclaimed rewards
+		{
+			d.toClient('S_CHAT', 2, {
+			channel: 21,
+			gm: 1,
+			authorName: 'Guardian Mission',
+			message: String(event.cleared + " / 40")
+			});
+		}
+		clr = event.cleared;
+	});
+}
